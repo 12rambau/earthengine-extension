389110a438cfa7b2272a36b2b018079c702a5939
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2026-07-21 11:55:57 +0000

    feat: start the sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+class EarthEngineTreeItem extends vscode.TreeItem {
+	constructor(label: string) {
+		super(label, vscode.TreeItemCollapsibleState.None);
+	}
+}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+class EarthEngineTreeDataProvider implements vscode.TreeDataProvider<EarthEngineTreeItem> {
+	getTreeItem(element: EarthEngineTreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(): EarthEngineTreeItem[] {
+		return [new EarthEngineTreeItem('Welcome to Earth Engine')];
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "earthengine" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	const treeDataProvider = new EarthEngineTreeDataProvider();
+	vscode.window.registerTreeDataProvider('earthengine.explorer', treeDataProvider);
+
 	const disposable = vscode.commands.registerCommand('earthengine.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from earthengine!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
