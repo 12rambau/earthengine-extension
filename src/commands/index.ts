21f1256501eccb631c841f0d0b660af9c6360523
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2026-07-21 12:17:20 +0000

    fix: add a status bar

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,5 +5,14 @@ export function registerCommands(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from earthengine!');
 	});
 
-	context.subscriptions.push(helloWorld);
+	const showAuthStatus = vscode.commands.registerCommand('earthengine.showAuthStatus', () => {
+		vscode.window.showWarningMessage(
+			'Google Earth Engine is not authenticated.',
+			'Sign in'
+		);
+	});
+	const signIn = vscode.commands.registerCommand('earthengine.signIn', () => {
+		vscode.window.showInformationMessage('Sign in to Google Earth Engine (not yet implemented)');
+	});
+	context.subscriptions.push(helloWorld, showAuthStatus);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,16 @@
 import * as vscode from 'vscode';
 import { EarthEngineTreeDataProvider } from './views/explorer';
 import { registerCommands } from './commands';
+import { AuthStatusBar } from './statusBar';
 
 export function activate(context: vscode.ExtensionContext) {
 	const treeDataProvider = new EarthEngineTreeDataProvider();
 	vscode.window.registerTreeDataProvider('earthengine.explorer', treeDataProvider);
 
 	registerCommands(context);
+
+	const authStatusBar = new AuthStatusBar();
+	context.subscriptions.push(authStatusBar);
 }
 
 export function deactivate() {}
